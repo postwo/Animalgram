63388c5ad24e57d1acccb242dc56a16bdfbefa69
postwo <codo7717@naver.com>
2025-12-20 17:08:09 +0900

    세팅

---
+++ src/main/java/com/example/Animalgram/AnimalgramApplication.java
@@ -0,0 +1,13 @@
+package com.example.Animalgram;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AnimalgramApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AnimalgramApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/Animalgram/AnimalgramApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.Animalgram;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AnimalgramApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
