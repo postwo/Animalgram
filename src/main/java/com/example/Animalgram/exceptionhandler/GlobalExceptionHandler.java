e61a3cac3b70eb6372a12c18f55583ce6d8ffeb1
postwo <codo7717@naver.com>
2026-02-12 21:19:14 +0900

    주석 삭제와 globalexception에 validation 에러 처리 추가

--- src/main/java/com/example/Animalgram/exceptionhandler/GlobalExceptionHandler.java
+++ src/main/java/com/example/Animalgram/exceptionhandler/GlobalExceptionHandler.java
@@ -6,13 +6,37 @@ import com.example.Animalgram.common.error.ErrorCode;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.core.annotation.Order;
 import org.springframework.http.ResponseEntity;
+import org.springframework.http.converter.HttpMessageNotReadableException;
+import org.springframework.validation.BindingResult;
+import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
+
+import java.util.stream.Collectors;
 
 @Slf4j
 @RestControllerAdvice
 @Order(value = Integer.MAX_VALUE)   // 가장 마지막에 실행 적용 // default가 max_value이다 지금은 그냥 명시한거다
 public class GlobalExceptionHandler {
+
+    @ExceptionHandler({MethodArgumentNotValidException.class, HttpMessageNotReadableException.class})
+    public ResponseEntity<Api<Object>> handleValidationExceptions(Exception ex) {
+        log.error("Validation error: ", ex);
+
+        if (ex instanceof MethodArgumentNotValidException) {
+            BindingResult bindingResult = ((MethodArgumentNotValidException) ex).getBindingResult();
+            String errorMessage = bindingResult.getFieldErrors().stream()
+                    .map(fieldError -> String.format("%s: %s", fieldError.getField(), fieldError.getDefaultMessage()))
+                    .collect(Collectors.joining(", "));
+            return ResponseEntity
+                    .status(400)
+                    .body(Api.ERROR(ErrorCode.BAD_REQUEST, errorMessage));
+        }
+
+        return ResponseEntity
+                .status(400)
+                .body(Api.ERROR(ErrorCode.BAD_REQUEST, "잘못된 요청 형식입니다."));
+    }
 
     //모든 exception 은 다 캐치
     @ExceptionHandler(value = Exception.class)
